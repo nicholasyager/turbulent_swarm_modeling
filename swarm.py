3f1d78b645c346ef3eafc2f0474c63c7f3b92408
Nicholas Yager <yager@nicholasyager.com>
2014-01-09 12:05:05 -0500

    Decreased the Gaussian distribution for cohesion and increased the power of the repulsion Weibull distribution. I still need to better handle cohesive ability so that it only accounts for nearby partners as opposed tot eh population as a whole. Also, I need to add some heading adjustments.

--- swarm.py
+++ swarm.py
@@ -16,7 +16,7 @@ class Fish:
     def __init__(self, width, height, depth, density):
         """Initialize a Fish object, giving it a random position at start."""
 
-        # Set a 
+        # Set a
 
         self.x = random.gauss(width/2, density)
         self.y = random.gauss(height/2, density)
@@ -42,10 +42,10 @@ class Fish:
         self.y += self.vector[1] * timestep
         self.z += self.vector[2] * timestep
 
-        print("Moving {0} x {1} x {2}".format(self.vector[0], 
-                                              self.vector[1], 
-                                              self.vector[2])
-                                              )
+        #print("Moving {0} x {1} x {2}".format(self.vector[0],
+        #                                      self.vector[1],
+        #                                      self.vector[2])
+        #                                      )
 
         return
 
@@ -69,10 +69,10 @@ class Fish:
 
 class Swarm:
     def __init__(self, n, d, r, width, height, depth, dampen):
-        """In a world of a given width, height, and depth, create a swarm of n 
-        size, with a start density d which equilibriates through swarm 
+        """In a world of a given width, height, and depth, create a swarm of n
+        size, with a start density d which equilibriates through swarm
         mentality to a desired radius r."""
-        
+
         self.r = r
 
         print("Creating a swarm of {0} with d={1}".format(n,d))
@@ -84,17 +84,17 @@ class Swarm:
             self.swarm.append(Fish(width, height, depth, d))
 
     def evaluate(self, w, timestep):
-        """Evaluate the swarm as individuals, choosing each randomly with 
+        """Evaluate the swarm as individuals, choosing each randomly with
         exclusion."""
 
         for locus in range(0,len(self.swarm)):
             vector = [0,0,0]
-            
+
             position = self.swarm[locus].getLocation()
 
             for fish in self.swarm:
                 location = fish.getLocation()
-             
+
 
                 for coord in range(0,3):
 
@@ -108,8 +108,8 @@ class Swarm:
                         direction = -direction
 
                     if magnatude > 0 :
-                        vector[coord] += direction * gaussian(magnatude, 5, 2) * 5
-                        vector[coord] += -direction * weibull(magnatude, 1, 0.125) / 20
+                        vector[coord] += direction * gaussian(magnatude, 3, 2) * 5
+                        vector[coord] += -direction * weibull(magnatude, 1, 0.125) / 18
                     else:
                         vector[coord] +=  0
 
@@ -117,15 +117,13 @@ class Swarm:
                 # In these intial stages, it may be best to use a form of
                 # circular current. Vector velocity is based on angular
                 # velocity w.
-        
+
             distance = math.sqrt( math.pow(5 - position[0],2) + \
                                  math.pow(5 - position[2],2))
 
             v = w *  gaussian(distance, 5, 3) * 5
             angle =  math.atan( (position[2] - 5 ) /  (position[0] - 5) )
 
-            print angle
-        
             # Quadrant II
             if position[0] < 5 and position[2] > 5:
                 angle = 3.14159 + angle
@@ -136,33 +134,29 @@ class Swarm:
 
             # Quadrant IV
             elif position[0] > 5 and position[2] < 5:
-                angle = (2 * 3.14159) + angle 
-
-            print angle
+                angle = (2 * 3.14159) + angle
 
             angle -= (3.14159/2)
 
-            print angle
-
             vector[0] += v * math.cos(angle)
             vector[2] +=  v * math.sin(angle)
-                
-            
-            print w, distance, v, (angle * 180)/3.14159, v *  math.cos(angle), v * math.sin(angle)
 
-                    
+
+            #print w, distance, v, (angle * 180)/3.14159, v *  math.cos(angle), v * math.sin(angle)
+
+
 
             self.swarm[locus].setVector(vector)
 
         for fish in self.swarm:
-        
+
             fish.move(timestep)
 
         return
 
 
     def write(self, t):
-        """Write the current state of the swarm at time t to a csv file of 
+        """Write the current state of the swarm at time t to a csv file of
         location."""
 
         if t < 100:
@@ -181,15 +175,15 @@ class Swarm:
         for fish in self.swarm:
             location = fish.getLocation()
             vector = fish.getVector()
-            swarmFile.write('{0},{1},{2},{3},{4},{5}\n'.format(location[0], 
-                                           location[1], 
+            swarmFile.write('{0},{1},{2},{3},{4},{5}\n'.format(location[0],
+                                           location[1],
                                            location[2],
                                            vector[0],
                                            vector[1],
                                            vector[2]))
-            print location
+            #print location
         swarmFile.close()
-        return 
+        return
 
 
 
@@ -198,11 +192,11 @@ WORLD_WIDTH = 10 # Arbitrarily set width
 WORLD_HEIGHT = 10
 WORLD_DEPTH = 10
 
-SIMULATION_TIMESTEP = 0.5
+SIMULATION_TIMESTEP = 0.1
 SIMULATION_TIME = 100
 SIMULATION_SPIN = 100
 
-SWARM_NUMBER_FISH = 1
+SWARM_NUMBER_FISH = 40
 SWARM_DENSITY = 4
 SWARM_RADIUS = 1
 SWARM_DAMPEN = 1
@@ -213,16 +207,16 @@ TIME = time.strftime("%Y-%m-%dT%H%M%S", time.gmtime())
 os.makedirs(TIME)
 
 # Create a swarm
-swarm = Swarm(SWARM_NUMBER_FISH, 
+swarm = Swarm(SWARM_NUMBER_FISH,
               SWARM_DENSITY,
               SWARM_RADIUS,
-              WORLD_WIDTH, 
-              WORLD_HEIGHT, 
+              WORLD_WIDTH,
+              WORLD_HEIGHT,
               WORLD_DEPTH,
               SWARM_DAMPEN)
 
 for tick in range(0,SIMULATION_TIME):
-    
+
     swarm.write(tick)
     swarm.evaluate(SIMULATION_SPIN,SIMULATION_TIMESTEP)
-    print("==========")
+    print("Tick: " + str(tick))
